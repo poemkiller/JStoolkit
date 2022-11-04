61c2ecabec62cc0f5d3c41b754f51fa217eb4db7
poemkilller <78832011+poemkiller@users.noreply.github.com>
2022-11-04 11:09:08 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(55, 55, 100);
+  rect(600, 400, 100, 100);
+  rect(500, 300, 100, 100);
+  
+  rect(400, 200, 100, 100);
+  rect(300, 100, 100, 100);
+  rect(700, 300, 100, 100);
+  rect(600, 200, 100, 100);
+}
